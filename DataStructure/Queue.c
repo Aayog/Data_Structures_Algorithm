aec86f8353b7d596ae197c3e6cecf75231a79179
Aayog Koirala <1aayogkoirala@gmail.com>
2023-05-01 19:58:45 -0400

    Added queue

--- DataStructure/Queue.c
+++ DataStructure/Queue.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node_struct {
+typedef struct node_struct
+{
     int data;
     struct node_struct *next;
 } node;
@@ -18,37 +19,42 @@ void flush_input_buffer();
 int menu();
 node *getNode(int num);
 
-int main() {
+int main()
+{
     int ch;
-    while (1) {
+    while (1)
+    {
         ch = menu();
         int num;
-        switch (ch) {
-            case 1:
-                printf("Enter number to enqueue: \n");
-                scanf("%d", &num);
-                enqueue(num);
-                break;
-            case 2:
-                dequeue();
-                break;
-            case 3:
-                display();
-                break;
-            case 4:
-                printf("The queue size is %d.\n", getSize());
-                break;
-            case 5:
-                exit(0);
-            default:
-                break;
+        switch (ch)
+        {
+        case 1:
+            printf("Enter number to enqueue: \n");
+            scanf("%d", &num);
+            enqueue(num);
+            break;
+        case 2:
+            dequeue();
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            printf("The queue size is %d.\n", getSize());
+            break;
+        case 5:
+            exit(0);
+        default:
+            break;
         }
     }
 }
 
-int menu() {
+int menu()
+{
     int choice;
-    do {
+    do
+    {
         printf("\n1) Enqueue a number\n");
         printf("2) Dequeue a number\n");
         printf("3) Display the queue\n");
@@ -60,32 +66,40 @@ int menu() {
     return choice;
 }
 
-node *getNode(int num) {
+node *getNode(int num)
+{
     node *temp = (node *)(malloc(sizeof(node)));
     temp->data = num;
     temp->next = NULL;
     return temp;
 }
 
-void enqueue(int number) {
+void enqueue(int number)
+{
     node *newnode = getNode(number);
-    if (rear == NULL) {
+    if (rear == NULL)
+    {
         front = rear = newnode;
-    } else {
+    }
+    else
+    {
         rear->next = newnode;
         rear = newnode;
     }
     size++;
 }
 
-void dequeue() {
-    if (front == NULL) {
+void dequeue()
+{
+    if (front == NULL)
+    {
         printf("Queue is empty.\n");
         return;
     }
     node *temp = front;
     front = front->next;
-    if (front == NULL) {
+    if (front == NULL)
+    {
         rear = NULL;
     }
     printf("%d dequeued from queue.\n", temp->data);
@@ -93,25 +107,31 @@ void dequeue() {
     size--;
 }
 
-void display() {
-    if (front == NULL) {
+void display()
+{
+    if (front == NULL)
+    {
         printf("Queue is empty.\n");
         return;
     }
     node *temp = front;
-    while (temp != NULL) {
+    while (temp != NULL)
+    {
         printf("%d ", temp->data);
         temp = temp->next;
     }
     printf("\n");
 }
 
-int getSize() {
+int getSize()
+{
     return size;
 }
 
-void flush_input_buffer() {
+void flush_input_buffer()
+{
     int c;
-    while ((c = getchar()) != '\n' && c != EOF) {
+    while ((c = getchar()) != '\n' && c != EOF)
+    {
     }
 }
